b76a6106beda68904350b363f6d73e6bfdb9304c
wingled22 <pelayowindel@gmail.com>
2024-02-15 12:50:13 +0800

    inject dep

--- Program.cs
+++ Program.cs
@@ -4,10 +4,24 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//enable cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
-
+//inject DB EF
 builder.Services.AddDbContext<GradTrackerContext>(options =>
     options.UseSqlServer("Server=localhost;Database=GradTracker;User ID=SA;Password=VeryStr0ngP@ssw0rd;TrustServerCertificate=true;"));
+
+builder.Services.AddControllers().AddJsonOptions(
+    options => {
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    }
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
