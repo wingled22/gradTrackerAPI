2e37a946ddeecc7d0ae4c4d40157cad28060551c
ademelmae <ademelviagedor22@gmail.com>
2024-02-22 10:50:55 +0800

    added alumnicontroller.cs

---
+++ Controllers/AlumniController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gradTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlumniController : ControllerBase
+    {
+        
+    }
+}
