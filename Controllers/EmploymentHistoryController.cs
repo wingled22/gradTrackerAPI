d64e87a11d66da2f5c85f23dbcba68cc7acbd365
sinadjanMJ <sinadjanmichaeljay@gmail.com>
2024-02-27 13:45:45 +0800

    partial code

--- Controllers/EmploymentHistoryController.cs
+++ Controllers/EmploymentHistoryController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using gradTrackerAPI.Entities;
-using gradTrackerAPI.ViewModel;
+// using gradTrackerAPI.ViewModel;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -21,64 +21,53 @@ namespace gradTrackerAPI.Controllers
             _context = context;
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<EmploymentHistory>> GetEmploymentHistory(int id)
-        {
-            if (_context.EmploymentHistories == null)
-            {
-                return NotFound();
-            }
-            var employmentHistory = await _context.EmploymentHistories.Where(e => e.AlumniId == id).ToListAsync();
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<EmploymentHistory>> GetEmploymentHistory(int id)
+        // {
+        //     if (_context.EmploymentHistories == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     var employmentHistory = await _context.EmploymentHistories.Where(e => e.AlumniId == id).ToListAsync();
+        //     // var employmentHistory = 
+        //     // (
+        //     //     from alum in _context.Alumni
+        //     //     join employment in _context.EmploymentHistories
+        //     //     on alum.Id equals employment.AlumniId
+        //     //     where id == employment.AlumniId
 
-            if (employmentHistory == null)
-            {
-                return NotFound();
-            }
+        //     //     select new AlumniEmployment
+        //     //     {
+        //     //         AlumniId = alum.Id,
+        //     //         Id = employment.Id,
+        //     //         CompanyName = employment.CompanyName,
+        //     //         Position = employment.Position,
+        //     //         StartDate = employment.StartDate,
+        //     //         EndDate = employment.EndDate,
+        //     //     }
 
-            return Ok(employmentHistory);
-        }
+        //     // ).ToList();
 
-        [HttpPost]
-        public async Task<ActionResult<EmploymentHistory>> PostEmploymentHistory(EmploymentHistory employmentHistory)
-        {
-            try
-            {
-                if (_context == null || _context.EmploymentHistories == null)
-                {
-                    return Problem("Context or entity set 'GradTrackerContext.EmploymentHistories' is null.");
-                }
+        //     if (employmentHistory == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-                _context.EmploymentHistories.Add(employmentHistory);
-                await _context.SaveChangesAsync();
+        //     return Ok(employmentHistory);
+        // }
 
-                return CreatedAtAction(nameof(GetEmploymentHistory), new { id = employmentHistory.Id }, employmentHistory);
-            }
-            catch (Exception ex)
-            {
-            
-                return StatusCode(500, "Internal server error");
-            }
-        }
+        // [HttpPost]
+        // public async Task<ActionResult<EmploymentHistory>> PostEmploymentHistory(EmploymentHistory employmentHistory)
+        // {
+        //     if (_context.EmploymentHistories == null)
+        //     {
+        //         return Problem("Entity set 'GradTrackerContext.Alumni'  is null.");
+        //     }
+        //     _context.EmploymentHistories.Add(employmentHistory);
+        //     await _context.SaveChangesAsync();
 
-        // DELETE: api/EmploymentHistory/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteEmploymentHistory(int id)
-        {
-            if (_context.EmploymentHistories == null)
-            {
-                return NotFound();
-            }
-
-            var employmentHistory = await _context.EmploymentHistories.FindAsync(id);
-            if (employmentHistory == null)
-            {
-                return NotFound();
-            }
-
-            _context.EmploymentHistories.Remove(employmentHistory);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
+        //     //return CreatedAtAction("GetAlumnus", new { id = alumnus.Id }, alumnus);
+        //     return Ok();
+        // }
     }
 }
